a303c7b5aa04c11d56e6b276de310009380034e1
Dmitriy Novikov <novikovds@tn.ru>
2024-03-18 22:31:11 +0300

    1) Добавили немного комментариев к env 2) Написали слой Policy (пока без проверки)

--- project/test.py
+++ project/test.py
@@ -2,7 +2,7 @@ import gymnasium
 
 from stable_baselines3 import PPO
 
-env = gymnasium.make("CartPole-v1", render_mode='rgb_array')
+env = gymnasium.make("CartPole-v1", render_mode='human')
 
 model = PPO("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=10_000)
